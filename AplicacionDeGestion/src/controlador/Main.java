cfd94bcd573efe5d93f6319ca42d047bfa0d82eb
Jorge <jorge@email.com>
2021-03-05 09:42:01 +0100

    primera version

---
+++ AplicacionDeGestion/src/controlador/Main.java
@@ -0,0 +1,25 @@
+package controlador;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Object o=new Object();
+		o.toString();
+		
+		String pepeEmilio=new String();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
